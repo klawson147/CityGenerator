988a5f43265273688f34bfbf8dbda846d2e6e612
klawson147 <kelbylawson@gmail.com>
2016-12-29 22:51:38 -0500

    Street Factory Declarations/Definitions

--- StreetFactory.cpp
+++ StreetFactory.cpp
@@ -9,3 +9,50 @@ StreetFactory::StreetFactory()
 StreetFactory::~StreetFactory()
 {
 }
+
+
+
+// Return The Slope from 2 Points
+float StreetFactory::calculateSlope(Point p1, Point p2)
+{
+	float m = p1.get_Y() - p2.get_Y();
+	m /= (p1.get_X() - p2.get_X());
+	return m;
+}
+
+// Return a Random X Value in the Range of a Street
+int StreetFactory::getPossibleXValue(Street s1)
+{
+	srand(time(NULL));
+
+	int xValue;
+
+	if (s1.getPointA().get_X() > s1.getPointB().get_X())
+	{
+		xValue = rand() % s1.getPointA().get_X() + s1.getPointB().get_X();
+	}
+	else
+	{
+		xValue = rand() % s1.getPointB().get_X() + s1.getPointA().get_X();
+	}
+
+	return xValue;
+
+}
+
+// Return a Random Point on the Street s1
+Point StreetFactory::getPointOnLine(Street s1)
+{
+	Point p1 = s1.getPointA();
+	Point p2 = s1.getPointB();
+
+	float slope = calculateSlope(p1, p2);
+
+	int newX = getPossibleXValue(s1);
+
+	float xVec = p1.get_X() - newX;
+
+	slope *= xVec;
+	slope -= p1.get_Y();
+
+}
--- StreetFactory.h
+++ StreetFactory.h
@@ -1,6 +1,11 @@
+#include <math.h>
+#include <time.h>
 #include "Street.h"
 #include "Directions.h"
+#include "Point.h"
 #pragma once
+
+
 class StreetFactory
 {
 public:
@@ -10,6 +15,8 @@ public:
 
 private:
 
-
+	Point getPointOnLine(Street);
+	float calculateSlope(Point, Point);
+	int getPossibleXValue(Street);
 };
 
